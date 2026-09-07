6d6aaea6cbb91869278cda1657af476fcd92ad6b
Alshebani Almorabet <alzarough@gmail.com>
2026-09-07 10:50:26 +0200

    Change title name

--- security_sentinel_360/__manifest__.py
+++ security_sentinel_360/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Odoo Sentinel 360: Access Matrix & Audit Suite",
+    "name": "All In One Access Management, Audit Trail & Session Security",
     "version": "18.0.1.0.0",
     "category": "Extra Tools",
     "summary": "Complete governance suite to track active logins, audit system changes, "
